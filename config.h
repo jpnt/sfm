5bd35523f94af5a4d2a6968eeac3974c81715b3a
João Pinto <joaopintojpgp@gmail.com>
2025-04-14 16:20:09 +0100

    switch pane using tab instead of space

--- config.h
+++ config.h
@@ -105,7 +105,7 @@ static Key nkeys[] = {
 	{ {.ch = 'P'},                 selmv,        {0}             },
 	{ {.ch = 'c'},                 start_change, {0}             },
 	{ {.ch = 'b'},                 opnsh,        {0}             },
-	{ {.key = TB_KEY_SPACE},       switch_pane,  {0}             },
+	{ {.key = TB_KEY_TAB},         switch_pane,  {0}             },
 	{ {.key = TB_KEY_CTRL_R},      refresh,      {0}             },
 	{ {.ch = '\\'},                bkmrk,        {.v = root}     },
 	{ {.ch = '.'},                 toggle_df,    {0}             },
